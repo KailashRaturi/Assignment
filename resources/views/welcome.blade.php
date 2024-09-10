7b3a4e079ea41a7d563efb3ffe8f1be54682f761
unknown <kailashraturi@8gmail.com>
2024-09-10 18:56:40 +0530

    Initial commit of Laravel app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Welcome to My Laravel App!</h1>
+    <p>This is a Home page.</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/', [WelcomeController::class, 'index']);
